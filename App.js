2497494c810c0468fe1050065603668b0f80cad1
Nathanael Bonfim <dev@nathabonfim59.com>
2024-05-02 02:47:19 -0300

    feat: add visual assets

--- App.js
+++ App.js
@@ -3,18 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Text>Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
